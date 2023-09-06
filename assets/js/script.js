f5b38c7b76c8818e4d9c7d3768dc5fea58d0129a
PRich57 <pcrichards57@gmail.com>
2023-09-06 15:49:24 -0600

    Fix genre discover method in findMatches function

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ $(function () {
     // Declare variable for rawg API key
     var rawgID = "?key=ad61e1d9ed3844018c1885a37313c3e9";
     // Declare variable for user input
-    var userFavorites = [$('#input-1'), $('#input-2'), $('#input-3')]
+    var userFavorites = ["God OF war", 'smash bros', 'the witcher']
 
     
     // FUNCTION DECLARATIONS
@@ -57,50 +57,71 @@ $("#home-button").on("click", function(event){
      //store search results
      //create variable to store searches in
 
-     var favGames = JSON.parse(localStorage.getItem("favorites"))|| [];
+    var favGames = JSON.parse(localStorage.getItem("favorites"))|| [];
 
-     function updateFave () {
-             favGames.forEach(function(game) {
-                 $("#favorites-list").append(`<li>${game}</li>`);
-             });
-         };
-
+    function updateFave () {
+            favGames.forEach(function(game) {
+                $("#favorites-list").append(`<li>${game}</li>`);
+            });
+        };
 
         var game =$("#input").val();
         favGames.unshift(game);
         localStorage.setItem("favorite-game", JSON.stringify(favGames));
         updateFave();
 
-     })
+    })
 
     
     // Declare findMatches function
     function findMatches () {
+        // Declare variable with all concatenated queries
+        var queries = "games" + rawgID + "&search_precise=true" + "&search_exact=true" + "&ordering=-metacritic" + "&dates=2020-01-01,2023-01-01" + "&search=";
+        // Declare genres variable to store genres of favorite games
+        var genres = [];
 
-        // userFavorites.forEach(element => {
+        userFavorites.forEach(element => {
             
             // Search input against database with game search endpoint
-            let requestSearch = rawgURL + "games" + rawgID + "&search=" + "god of war" + "&search_exact=true";
+            let requestSearch = rawgURL + queries + element;
             console.log(requestSearch);
-        // });
+        
             fetch(requestSearch)
             .then(function (response) {
-                if (response.ok) {
-                    return response.json();
-                } else if (response.status === 404) {
+                // Validation
+                if (response.status === 404) {
                     return;
                 }
+                return response.json();
             })
             .then(function (data) {
                 console.log(data);
+                var results = data.results;
+                console.log(results);
                 // Get genre and store in variable
+                for (var i = 0; i < userFavorites.length; i++) {
+                    var namesLower = userFavorites[i].toLowerCase();
+                    for (var j = 0; j < results.length; j++) {
+                        var resultsLower = results[j].name.toLowerCase();
+                        if (resultsLower.includes(namesLower) && results[j].genres.length > 0) {
+                            
+                            genres.push(results[j].genres[0].name);
+                        }
+                        
+                    }
+                }
+                // remove duplicates
+                genres = [...new Set(genres)];
                 
+                // search user's genres for top games of same genres
+                console.log(genres);
+
+                });
                 // var userGenre = data.genreElement
                 
-                // search userGenre for top games of same genre
+            });
                 
-                
-            })
+            
     }
     
     
